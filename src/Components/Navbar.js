8c1f12da35d9004d75ed1a03ca5956e5f778a054
Manisekar <mani.bio.197@gmail.com>
2021-10-29 22:20:39 +0530

    test

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,29 +6,29 @@ const Navbar = () => {
         <div>
             
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Start Bootstrap
+  <a class="navbar-brand" href="#a">Start Bootstrap
 </a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#anavbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="#a">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
+        <a class="nav-link" href="#a">Link</a>
       </li>
       <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <a class="nav-link dropdown-toggle" href="#a" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Shop
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">    All Products</a>
-          <a class="dropdown-item" href="#">Popular Items</a>
+          <a class="dropdown-item" href="#a">    All Products</a>
+          <a class="dropdown-item" href="#a">Popular Items</a>
           <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">New Arrivals</a>
+          <a class="dropdown-item" href="#a">New Arrivals</a>
         </div>
       </li>
   
